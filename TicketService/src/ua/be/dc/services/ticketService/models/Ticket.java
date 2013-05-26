9b3233290bc20a89c8b16f796db2e6d513785f7d
Pau Pérez <saulopefa@gmail.com>
2013-05-26 23:36:51 +0200

    Add toString method and remove seat and ticket relation

--- TicketService/src/ua/be/dc/services/ticketService/models/Ticket.java
+++ TicketService/src/ua/be/dc/services/ticketService/models/Ticket.java
@@ -1,19 +1,9 @@
 package ua.be.dc.services.ticketService.models;
 
-import java.rmi.RemoteException;
-
-import javax.xml.rpc.ServiceException;
-
-import ua.be.dc.services.seatAccommodation.service.Seat;
-
-import ua.be.dc.services.seatAccommodation.service.SeatAccommodationService;
-import ua.be.dc.services.seatAccommodation.service.SeatAccommodationServiceImplServiceLocator;
-
 public class Ticket {
 
 	private Integer id;
 	private Event event;
-	private Integer seatId;
 	private Channel channel;
 	private Float price;
 	private Boolean sold;
@@ -73,33 +63,7 @@ public class Ticket {
 		this.available = available;
 	}
 
-	public Integer getSeatId() {
-		return seatId;
-	}
-
-	public void setSeatId(Integer seatId) {
-		this.seatId = seatId;
-	}
-	
-	/**
-     * Returns a Seat instance from the Seat Accommodation Service
-     * @return A Seat with an id value equals to this.id, null otherwise
-	 * @throws ServiceException 
-	 * @throws RemoteException 
-     */
-	public Seat getSeat() throws ServiceException, RemoteException {
-		SeatAccommodationServiceImplServiceLocator serviceLocator = new SeatAccommodationServiceImplServiceLocator();
-		SeatAccommodationService seatAccommodationService;
-		
-		Seat seat = new Seat();
-		seatAccommodationService = serviceLocator.getSeatAccommodationServiceImplPort();
-		seat = seatAccommodationService.getSeatById(this.id);
-		
-		return seat;
-	}
-	
 	public String toString() {
-		return "Ticket: [id = " + id + ", event = " + event.getId() + ", seatId = " + seatId 
-			 + ", channel = " + channel.getId() + ", price = " + price + ", sold = " + sold + ", available = " + available + "]";
+		return "Ticket: [id = " + id + ", event = " + event.getId() + ", channel = " + channel.getId() + ", price = " + price + ", sold = " + sold + ", available = " + available + "]";
 	}
 }
