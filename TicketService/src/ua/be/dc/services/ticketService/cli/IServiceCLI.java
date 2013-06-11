22ea2ec1946a7b0bf11cb5d7dd9219728859919e
Pau Pérez <saulopefa@gmail.com>
2013-06-11 14:21:36 +0200

    Working version of TicketServiceCLI for ticketws

--- TicketService/src/ua/be/dc/services/ticketService/cli/IServiceCLI.java
+++ TicketService/src/ua/be/dc/services/ticketService/cli/IServiceCLI.java
@@ -12,4 +12,9 @@ public interface IServiceCLI {
 	public void executeStatement(String[] args);
 	
 	public void showHelp();
+	
+	/**
+	 * Assumes the existence of a shell script wrapper
+	 */
+	public void showUsage();
 }
--- TicketService/src/ua/be/dc/services/ticketService/cli/TicketServiceCLI.java
+++ TicketService/src/ua/be/dc/services/ticketService/cli/TicketServiceCLI.java
@@ -2,9 +2,13 @@ package ua.be.dc.services.ticketService.cli;
 
 import gnu.getopt.Getopt;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import ua.be.dc.services.ticketService.cli.util.Utils;
 import ua.be.dc.services.ticketService.manager.ITicketServiceManager;
 import ua.be.dc.services.ticketService.manager.TicketServiceManager;
 import ua.be.dc.services.ticketService.models.Channel;
@@ -15,7 +19,7 @@ public class TicketServiceCLI implements IServiceCLI {
 	
 	private static Logger logger = LogManager.getLogger(TicketServiceCLI.class.getName());
 	
-	private Ticket ticket;
+	private static final int TICKET_NUM_FIELDS = 5;
 	private ITicketServiceManager serviceManager;
 	
 	public TicketServiceCLI() {
@@ -25,22 +29,25 @@ public class TicketServiceCLI implements IServiceCLI {
 	@Override
 	public void executeStatement(String[] args) {
 		int cmd;
-		Getopt g = new Getopt("ticketws tickets", args, "a:r:h:l");
+		Getopt g = new Getopt("ticketws tickets", args, "r:h:t:lea");
 		while ((cmd = g.getopt()) != -1) {
 			switch (cmd) {
 			case 'l':
 				listTickets();
 				break;
 			case 'a':
-				addTicket(g);
+				args = Arrays.copyOfRange(args, 1, args.length);
+				addTicket(args);
 				break;
 			case 'r':
 				removeTicket(g);
 				break;
+			case 'e':
+				emptyTickets();
+				break;
 			case 'h':
 				showHelp();
 				break;
-				
 			default:
 			case '?':
 				showHelp();
@@ -49,13 +56,33 @@ public class TicketServiceCLI implements IServiceCLI {
 		}
 	}
 	
+	/**
+	 * 1- cd /Applications/eclipse_dc/plugins/oracle.eclipse.runtime.glassfish_3.1.0.0/glassfish3/bin
+	 * 2- ./asadmin
+	 * 3- list-domains
+	 * 4- asadmin> list-domains
+	 * 5- asadmin> create-domain --adminport <port_num> <domain_name>
+	 * 6- New > Server pointing to the new domain folder
+	 * 7- deploy service and get endpoint through domain's glassfish admin console (same as wsdl)
+	 */
+	
+//	private void setEndpoint(Getopt g) {
+//		String address = String.valueOf(g.getOptarg());
+//		
+//		System.out.println("address = " + address);
+//		
+//		serviceManager.setEndpointAddress(address);
+//		System.out.println("num seats = " + serviceManager.test());
+//	}
+
 	private void listTickets() {
-		System.out.println("ticket id, event id, seat id, channel id, price, sold, available");
+		System.out.println("ticket id, event id, channel id, price, sold, available");
 		System.out.println("----------------------------------------------------------------");
-		for (Ticket ticket : serviceManager.getTickets()) {
+		
+		List<Ticket> tickets = serviceManager.getTickets();
+		for (Ticket ticket : tickets) {
 			System.out.println(ticket.getId() + ", "
 							 + ticket.getEvent().getId() + ", "
-							 + ticket.getSeatId() + ", "
 							 + ticket.getChannel().getId() + ", "
 							 + ticket.getPrice() + ", "
 							 + ticket.getSold() + ", "
@@ -63,29 +90,57 @@ public class TicketServiceCLI implements IServiceCLI {
 		}
 	}
 
-	public void addTicket(Getopt g) {
+	public void addTicket(String[] args) {
 		try {
-			ticket = new Ticket();
-			ticket.setEvent(new Event(Integer.valueOf(g.getOptarg())));
-			ticket.setSeatId(Integer.valueOf(g.getOptarg()));
-			ticket.setChannel(new Channel(Integer.valueOf(g.getOptarg())));
-			ticket.setPrice(Float.valueOf(g.getOptarg()));
-			ticket.setSold(Boolean.valueOf(g.getOptarg()));
-			ticket.setAvailable(Boolean.valueOf(g.getOptarg()));
+			String[] fields = Utils.parseFields(args);
+			if (args.length != TICKET_NUM_FIELDS) {
+				showHelp();
+				System.exit(-1);
+			}
+			
+			Integer eventId = Integer.valueOf(fields[0]);
+			Integer channelId = Integer.valueOf(fields[1]);
+			Float price = Float.valueOf(fields[2]);
+			Boolean sold = Boolean.valueOf(fields[3]);
+			Boolean available = Boolean.valueOf(fields[4]);
+			
+			Ticket ticket = new Ticket();
+			ticket.setEvent(new Event(eventId));
+			ticket.setChannel(new Channel(channelId));
+			ticket.setPrice(price);
+			ticket.setSold(sold);
+			ticket.setAvailable(available);
 			
 			serviceManager.createTicket(ticket);
 			
 			logger.trace("CLI: Added ticket with ID " + ticket.getId());
-		} catch (IllegalArgumentException e) {
+		} catch (Exception e) {
 			System.out.println("The ticket couldn't be added. " + e.getMessage());
 		}
 	}
 	
 	public void removeTicket(Getopt g) {
-		Integer ticketId = Integer.valueOf(g.getOptarg());
-		serviceManager.deleteTicketById(ticketId);
-
-		logger.trace("CLI: Removed ticket with ID " + ticketId);
+		try {
+			Integer ticketId = Integer.valueOf(g.getOptarg());
+			serviceManager.deleteTicketById(ticketId);
+			
+			logger.trace("CLI: Removed ticket with ID " + ticketId);
+		} catch (Exception e) {
+			System.err.println("CLI: " + e.getMessage());
+		}
+	}
+	
+	public void emptyTickets() {
+		try {
+			List<Ticket> tickets = serviceManager.getTickets();
+			for (Ticket ticket : tickets) {
+				serviceManager.deleteTicketById(ticket.getId());
+			}
+			
+			logger.trace("CLI: All tickets deleted");
+		} catch (Exception e) {
+			System.err.println("CLI: " + e.getMessage());
+		}
 	}
 
 	/**
@@ -103,8 +158,9 @@ public class TicketServiceCLI implements IServiceCLI {
 		
 		System.out.println("The tickets commands are:");
 		System.out.println("   -l  List all the tickets");
-		System.out.println("   -a  Add a ticket to the system. You must provide the following parameters: event id, seat id, channel id, price, sold and available");
-		System.out.println("   -r  Remove an event from the system. You must provide the event id");
+		System.out.println("   -a  Add a ticket to the system. You must provide the information as following: <event_id>, <channel_id>, <price>, <sold>, <available>");
+		System.out.println("   -r  Remove a ticket from the system. You must provide the ticket id");
+		System.out.println("   -e  Remove all tickets from the system");
 		System.exit(1);
 	}
 
