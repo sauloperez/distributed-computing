191db1a589d0a73ffb2643f64add3884a32c6bfd
Pau Pérez <saulopefa@gmail.com>
2013-06-12 11:45:19 +0200

    CLI Utils needed for commit acf75200e25bd081a76429f74e05af5321978104

---
+++ SeatAccommodationService/src/ua/be/dc/services/seatAccommodation/cli/util/Constants.java
@@ -0,0 +1,10 @@
+package ua.be.dc.services.seatAccommodation.cli.util;
+
+public class Constants {
+
+	// Input formats and characters
+	public static String INPUT_DATE_FORMAT = "yyyy-MM-dd hh:mm";
+	
+	public static String INPUT_SEPARATOR = ",";
+	
+}
---
+++ SeatAccommodationService/src/ua/be/dc/services/seatAccommodation/cli/util/Utils.java
@@ -0,0 +1,53 @@
+package ua.be.dc.services.seatAccommodation.cli.util;
+
+import java.util.ArrayList;
+
+public class Utils {
+
+	/**
+	 * Returns a list of multiple-word command line arguments separated by a certain separator char
+	 * @param fields
+	 * @return
+	 */
+	public static String[] parseFields(String[] fields) {
+		ArrayList<String> fieldsList = new ArrayList<>();
+		
+		if (fields.length == 1) {
+			fieldsList.add(fields[0]);
+		}
+		else {
+			int i = 0;
+			while (i < fields.length) {
+				
+				String field = "";
+				for (int j = i; j < fields.length; j++) {
+					String str = fields[j];
+					String lastChar = str.substring(str.length() - 1);
+					
+					// Stop building the field when we reach a separator char
+					if (str.equals(Constants.INPUT_SEPARATOR) || lastChar.equals(Constants.INPUT_SEPARATOR)) {
+						str = str.substring(0, str.length()-1);
+						field += str;
+						
+						i = j+1;
+						break;
+					}
+					else {
+						// add whitespace except for the last word
+						if (j < fields.length-1) {
+							str += " ";
+						}
+					}
+					
+					field += str;
+					i = j+1;
+				}
+				
+				// once built add it to the list of fields
+				fieldsList.add(field);
+			}
+		}
+		
+		return fieldsList.toArray(new String[fieldsList.size()]);
+	}
+}
